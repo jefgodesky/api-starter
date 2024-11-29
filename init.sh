507e9b7ba9e70bd107520840cd5c741efd69a0c4
Jason Godesky <jefgodesky@gmail.com>
2024-11-29 18:20:13 -0500

    Fix dev environment

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-for sql_file in /docker-entrypoint-initdb.d/collections/**/*.sql; do
+set -e
+for sql_file in /scripts/collections/**/init.sql; do
   echo "Running $sql_file"
-  psql -U "$POSTGRES_USER" -f "$sql_file"
+  psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f "$sql_file"
 done
