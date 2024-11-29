d0ac788dfc004ecb9eff2bc2d4ff3a285fee7bde
Jason Godesky <jefgodesky@gmail.com>
2024-11-29 14:18:01 -0500

    Add new utility method: getPaginationQueries

---
+++ utils/get-pagination-queries.test.ts
@@ -0,0 +1,53 @@
+import { describe, it } from 'jsr:@std/testing/bdd'
+import { expect } from 'jsr:@std/expect'
+import getPaginationQueries from './get-pagination-queries.ts'
+
+describe('getPaginationQueries', () => {
+  it('handles an empty set', () => {
+    const queries = getPaginationQueries(0, 0, 10)
+    expect(queries.first).toBe('offset=0&limit=10')
+    expect(queries.prev).toBe('offset=0&limit=10')
+    expect(queries.next).toBe('offset=0&limit=10')
+    expect(queries.last).toBe('offset=0&limit=10')
+  })
+
+  it('handles a set with just one page', () => {
+    const queries = getPaginationQueries(8, 0, 10)
+    expect(queries.first).toBe('offset=0&limit=10')
+    expect(queries.prev).toBe('offset=0&limit=10')
+    expect(queries.next).toBe('offset=0&limit=10')
+    expect(queries.last).toBe('offset=0&limit=10')
+  })
+
+  it('handles the first page of a set', () => {
+    const queries = getPaginationQueries(175, 0, 10)
+    expect(queries.first).toBe('offset=0&limit=10')
+    expect(queries.prev).toBe('offset=0&limit=10')
+    expect(queries.next).toBe('offset=10&limit=10')
+    expect(queries.last).toBe('offset=170&limit=10')
+  })
+
+  it('handles the x+1 page of a set', () => {
+    const queries = getPaginationQueries(175, 30, 10)
+    expect(queries.first).toBe('offset=0&limit=10')
+    expect(queries.prev).toBe('offset=20&limit=10')
+    expect(queries.next).toBe('offset=40&limit=10')
+    expect(queries.last).toBe('offset=170&limit=10')
+  })
+
+  it('handles weird offsets', () => {
+    const queries = getPaginationQueries(175, 42, 10)
+    expect(queries.first).toBe('offset=0&limit=10')
+    expect(queries.prev).toBe('offset=30&limit=10')
+    expect(queries.next).toBe('offset=50&limit=10')
+    expect(queries.last).toBe('offset=170&limit=10')
+  })
+
+  it('handles the last page of a set', () => {
+    const queries = getPaginationQueries(175, 172, 10)
+    expect(queries.first).toBe('offset=0&limit=10')
+    expect(queries.prev).toBe('offset=160&limit=10')
+    expect(queries.next).toBe('offset=170&limit=10')
+    expect(queries.last).toBe('offset=170&limit=10')
+  })
+})
