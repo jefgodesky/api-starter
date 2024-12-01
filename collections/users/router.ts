21dd20c6b393da523aa3376f29970ee6567a6611
Jason Godesky <jefgodesky@gmail.com>
2024-11-30 20:24:27 -0500

    Implement middleware, controller

--- collections/users/router.ts
+++ collections/users/router.ts
@@ -1,48 +1,31 @@
-import * as uuid from '@std/uuid'
 import { Router } from '@oak/oak'
-import User from './model.ts'
-import {
-  isUserCreation,
-  makeUserResponse,
-  allUserAttributes,
-  getUserFields
-} from './resource.ts'
-import UserRepository from './repository.ts'
+import UserController from './controller.ts'
 import getPrefix from '../../utils/get-prefix.ts'
 import sendJSON from '../../utils/responses/send-json.ts'
-import { send400, send404, send500 } from '../../utils/responses/errors.ts'
+import { send404, send500 } from '../../utils/responses/errors.ts'
+import requireUserCreationBody from '../../middlewares/require/body/user-creation.ts'
 
-const repository = new UserRepository()
 const router = new Router({
   methods: ['POST'],
   prefix: getPrefix('users')
 })
 
-router.post('/', async ctx => {
-  const { data } = await ctx.request.body.json()
-  if (!isUserCreation({ data })) {
-    send400(ctx)
-  } else {
-    const user = data.attributes as User
-    const saved = await repository.save(user)
-    if (saved.id) {
-      sendJSON(ctx, makeUserResponse(saved, allUserAttributes))
-    } else {
-      send500(ctx)
-    }
+router.post('/', requireUserCreationBody, async ctx => {
+  const body = await ctx.request.body.json()
+  try {
+    const user = await UserController.create(body)
+    sendJSON(ctx, user)
+  } catch (_) {
+    send500(ctx)
   }
 })
 
 router.get('/:id', async ctx => {
-  const { id } = ctx.params
-  const user = uuid.v4.validate(id)
-    ? await repository.get(id)
-    : await repository.getByUsername(id)
-
-  if (!user) {
-    send404(ctx)
+  const user = await UserController.get(ctx.params.id, ctx)
+  if (user) {
+    sendJSON(ctx, user)
   } else {
-    sendJSON(ctx, makeUserResponse(user, getUserFields(ctx)))
+    send404(ctx)
   }
 })
 
