8307e28c4458211fda4b985b052f3508ba3c14cf
Jason Godesky <jefgodesky@gmail.com>
2024-12-03 20:17:00 -0500

    Set Content-Type: application/vnd.api+json

--- utils/responses/send-json.ts
+++ utils/responses/send-json.ts
@@ -3,7 +3,7 @@ import type { Response } from '../../jsonapi.d.ts'
 
 const sendJSON = (ctx: Context, content: Response): void => {
   ctx.response.status = Status.OK
-  ctx.response.type = 'json'
+  ctx.response.type = 'application/vnd.api+json'
   ctx.response.body = content
 }
 
