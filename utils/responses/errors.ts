2d5e2943d8ff506cd4c80398a6f98c1768b29065
Jason Godesky <jefgodesky@gmail.com>
2024-12-04 18:06:05 -0500

    Add new error: send415

--- utils/responses/errors.ts
+++ utils/responses/errors.ts
@@ -1,9 +1,11 @@
 import send400 from './send-400.ts'
 import send404 from './send-404.ts'
+import send415 from './send-415.ts'
 import send500 from './send-500.ts'
 
 export {
   send400,
   send404,
+  send415,
   send500
 }
