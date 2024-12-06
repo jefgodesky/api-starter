095027b4d6e8d90f26ee697556a45d92dbcdec21
Jason Godesky <jefgodesky@gmail.com>
2024-12-06 17:26:13 -0500

    Move to a more typical, numbered migrations approach

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-for sql_file in /scripts/collections/**/init.sql; do
+for sql_file in /scripts/migrations/*.sql; do
   echo "Running $sql_file"
   psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f "$sql_file"
 done
