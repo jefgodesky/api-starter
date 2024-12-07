750a101b8080e0ee62e45b838b3da2d0f384216b
Jason Godesky <jefgodesky@gmail.com>
2024-12-06 20:00:47 -0500

    Add new utility method: readableStreamToString

--- utils/string-to-readable-stream.test.ts
+++ utils/string-to-readable-stream.test.ts
@@ -1,23 +1,10 @@
 import { describe, it } from 'jsr:@std/testing/bdd'
 import { expect } from 'jsr:@std/expect'
+import readableStreamToString from './readable-stream-to-string.ts'
 import stringToReadableStream from './string-to-readable-stream.ts'
 
 describe('stringToReadableStream', () => {
   const greeting = 'Hello, world!'
-  const readableStreamToString = async (stream: ReadableStream<any>): Promise<string> => {
-    const reader = stream.getReader()
-    const decoder = new TextDecoder()
-    let result = ''
-
-    while (true) {
-      const { done, value } = await reader.read()
-      if (done) break
-      result += decoder.decode(value, { stream: true })
-    }
-
-    result += decoder.decode()
-    return result
-  }
 
   it('returns a readable stream', async () => {
     const stream = stringToReadableStream(greeting)
