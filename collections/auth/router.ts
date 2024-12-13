565d05a5c7d403f0d03ada8e36be02e4f771c438
Jason Godesky <jefgodesky@gmail.com>
2024-12-13 16:26:14 -0500

    GET /v1/auth/providers

--- collections/auth/router.ts
+++ collections/auth/router.ts
@@ -1,9 +1,37 @@
 import { Router } from '@oak/oak'
+import type ProviderResource from '../../types/provider-resource.ts'
+import { PROVIDERS } from '../../types/provider.ts'
 import RootRouter from '../base/router.ts'
 import TokenRouter from './tokens/router.ts'
 
+import getJSONAPI from '../../utils/get-jsonapi.ts'
+import getRoot from '../../utils/get-root.ts'
+import sendJSON from '../../utils/responses/send-json.ts'
+
+const providers = new Router({
+  methods: ['GET'],
+  prefix: '/providers'
+})
+
+providers.get('/', ctx => {
+  const providers: ProviderResource[] = []
+  for (const id of Object.values(PROVIDERS)) {
+    providers.push({ type: 'provider', id })
+  }
+
+  sendJSON(ctx, {
+    jsonapi: getJSONAPI(),
+    links: {
+      self: getRoot() + '/auth/providers',
+      describedBy: getRoot() + '/docs'
+    },
+    data: providers
+  })
+})
+
 const routers: Record<string, Router> = {
-  tokens: TokenRouter
+  tokens: TokenRouter,
+  providers
 }
 
 const root = new RootRouter(routers, 'auth')
