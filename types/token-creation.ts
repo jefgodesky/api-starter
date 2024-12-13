9646ff71eafa7f3b7291592e8da869944aca9740
Jason Godesky <jefgodesky@gmail.com>
2024-12-12 20:56:30 -0500

    Fix TokenCreation
    Add unit tests, provide options for creating a new access token or refreshing an existing one.

--- types/token-creation.ts
+++ types/token-creation.ts
@@ -1,9 +1,11 @@
+import { PROVIDERS } from './provider.ts'
+import type TokenAccessAttributes from './token-access-attributes.ts'
+import type TokenRefreshAttributes from './token-refresh-attributes.ts'
+
 export default interface TokenCreation {
   data: {
     type: 'tokens'
-    attributes: {
-      token: string
-    }
+    attributes: TokenAccessAttributes | TokenRefreshAttributes
   }
 }
 
@@ -17,7 +19,8 @@ const isTokenCreation = (candidate: any): candidate is TokenCreation => {
   const { type, attributes } = data
   if (type !== 'tokens') return false
 
-  const { token } = attributes
+  const { token, provider } = attributes
+  if (provider !== undefined && !Object.values(PROVIDERS).includes(provider)) return false
   return typeof token === 'string'
 
 }
