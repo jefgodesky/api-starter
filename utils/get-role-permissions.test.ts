854f55added4e9322c7a77ab093232fb0abd8010
Jason Godesky <jefgodesky@gmail.com>
2024-12-19 20:22:52 -0500

    getRolePermissions: Return anonymous permissions if given no parameter

--- utils/get-role-permissions.test.ts
+++ utils/get-role-permissions.test.ts
@@ -4,6 +4,12 @@ import { ROLES } from '../types/role.ts'
 import getRolePermissions from './get-role-permissions.ts'
 
 describe('getRolePermissions', () => {
+  it('returns anonymous permissions if given no parameter', async () => {
+    const actual = await getRolePermissions()
+    expect(Array.isArray(actual)).toBe(true)
+    expect(actual.length).toBeGreaterThan(0)
+  })
+
   it('returns the permissions associated with a given role', async () => {
     const actual = await getRolePermissions(ROLES.ACTIVE)
     expect(Array.isArray(actual)).toBe(true)
--- utils/get-role-permissions.ts
+++ utils/get-role-permissions.ts
@@ -1,8 +1,9 @@
 import { parse } from '@std/yaml'
 import type Role from '../types/role.ts'
 
-const getRolePermissions = async (role: Role): Promise<string[]> => {
-  const yaml = await Deno.readTextFile(`/app/roles/${role}.yml`)
+const getRolePermissions = async (role?: Role): Promise<string[]> => {
+  const path = role ? `/app/roles/${role}.yml` : '/app/roles/anonymous.yml'
+  const yaml = await Deno.readTextFile(path)
   // deno-lint-ignore no-explicit-any
   const data = parse(yaml) as any
   return ('permissions' in data) ? data.permissions : []
