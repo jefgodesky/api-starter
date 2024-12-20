9ae6133c3661e3f2ec0632b6dcac5a558d765c45
Jason Godesky <jefgodesky@gmail.com>
2024-12-19 20:26:53 -0500

    getPermissions: Everyone gets anonymous permissions

--- utils/get-permissions.test.ts
+++ utils/get-permissions.test.ts
@@ -5,6 +5,12 @@ import getRolePermissions from './get-role-permissions.ts'
 import getPermissions from './get-permissions.ts'
 
 describe('getPermissions', () => {
+  it('returns anonymous permissions if given no parameter', async() => {
+    const actual = await getPermissions()
+    const anon = await getRolePermissions()
+    expect(actual).toEqual(anon)
+  })
+
   it('returns the permissions that a user has', async() => {
     const actual = await getPermissions({
       id: crypto.randomUUID(),
@@ -12,9 +18,11 @@ describe('getPermissions', () => {
       roles: [ROLES.ACTIVE, ROLES.ADMIN]
     })
 
+    const anon = await getRolePermissions()
     const active = await getRolePermissions(ROLES.ACTIVE)
     const admin = await getRolePermissions(ROLES.ADMIN)
 
+    expect(anon.every(p => actual.includes(p))).toBe(true)
     expect(active.every(p => actual.includes(p))).toBe(true)
     expect(admin.every(p => actual.includes(p))).toBe(true)
     expect(new Set(actual).size).toBe(actual.length) // No duplicates
