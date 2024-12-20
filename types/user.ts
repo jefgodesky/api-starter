526d8afcb685a78cdc0d74f9c3a035e847997970
Jason Godesky <jefgodesky@gmail.com>
2024-12-19 19:46:29 -0500

    Add new type guard: isUser

--- types/user.ts
+++ types/user.ts
@@ -1,8 +1,31 @@
 import type Model from './model.ts'
 import type Role from './role.ts'
+import { isRole } from './role.ts'
 
 export default interface User extends Model {
   name: string
   username?: string
   roles?: Role[]
 }
+
+// deno-lint-ignore no-explicit-any
+const isUser = (candidate: any): candidate is User => {
+  if (typeof candidate !== 'object') return false
+  if (typeof candidate?.name !== 'string') return false
+
+  const permitted = ['id', 'name', 'username', 'roles']
+  if (!Object.keys(candidate).every(key => permitted.includes(key))) return false
+
+  const noUsername = candidate.username === undefined
+  const strUsername = typeof candidate.username === 'string'
+  const noRoles = candidate.roles === undefined
+  // deno-lint-ignore no-explicit-any
+  const allRoles = Array.isArray(candidate.roles) && candidate.roles.every((role: any) => isRole(role))
+
+  if (!noUsername && !strUsername) return false
+  if (!noRoles && !allRoles) return false
+
+  return true
+}
+
+export { isUser }
