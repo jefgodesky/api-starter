acf80e7f46140d421a52126bc372e4830e654682
Jason Godesky <jefgodesky@gmail.com>
2024-12-19 20:42:24 -0500

    authTokenToJWT: Base exp on token, not default value

--- utils/transformers/auth-token-to-jwt.ts
+++ utils/transformers/auth-token-to-jwt.ts
@@ -1,13 +1,12 @@
 import { signJWT } from '@cross/jwt'
 import type AuthToken from '../../types/auth-token.ts'
 import getJWTSecret from '../get-jwt-secret.ts'
-import getTokenExpiration from '../get-token-expiration.ts'
 
 const authTokenToJWT = async (token: AuthToken): Promise<string> => {
   return await signJWT({
     ...token,
     sub: token.user.id,
-    exp: getTokenExpiration().getTime() / 1000
+    exp: Math.floor(token.expiration.token.getTime() / 1000)
   }, getJWTSecret(), { setIat: true })
 }
 
