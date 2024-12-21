fcc683b0bb478a46f6d10c8f5c494c069210d6b8
Jason Godesky <jefgodesky@gmail.com>
2024-12-21 14:26:45 -0500

    /accounts — Rewrite to throw errors

--- collections/accounts/router.ts
+++ collections/accounts/router.ts
@@ -1,9 +1,8 @@
-import { Router } from '@oak/oak'
+import { Router, Status, createHttpError } from '@oak/oak'
 import Provider, { isProvider } from '../../types/provider.ts'
 import AccountController from './controller.ts'
 import sendJSON from '../../utils/responses/send-json.ts'
-import send204 from '../../utils/responses/send-204.ts'
-import { send400, send500 } from '../../utils/responses/errors.ts'
+import getMessage from '../../utils/get-message.ts'
 import getPrefix from '../../utils/get-prefix.ts'
 import addClient from '../../middlewares/add-client.ts'
 import requireClient from '../../middlewares/require/client.ts'
@@ -16,31 +15,18 @@ const router = new Router({
 router.post('/', addClient, requireClient, requireTokenCreationBody, async ctx => {
   const body = await ctx.request.body.json()
   const { provider, token } = body.data.attributes
+  const err = createHttpError(Status.BadRequest, getMessage('invalid_account_post'))
+  if (!provider || !token) throw err
 
-  if (!provider || !token) {
-    send400(ctx)
-    return
-  }
-
-  try {
-    const res = await AccountController.create(ctx.state.client.id, provider, token)
-    if (res) {
-      sendJSON(ctx, res)
-    } else {
-      send400(ctx)
-    }
-  } catch (_) {
-    send400(ctx)
-  }
+  const res = await AccountController.create(ctx.state.client.id, provider, token)
+  if (!res) throw err
+  sendJSON(ctx, res)
 })
 
 router.get('/', addClient, requireClient, async ctx => {
   const res = await AccountController.list(ctx.state.client.id)
-  if (res) {
-    sendJSON(ctx, res)
-  } else {
-    send500(ctx)
-  }
+  if (!res) throw createHttpError(Status.InternalServerError)
+  sendJSON(ctx, res)
 })
 
 router.get('/:provider', addClient, requireClient, async ctx => {
@@ -48,11 +34,8 @@ router.get('/:provider', addClient, requireClient, async ctx => {
   const res = provider
     ? await AccountController.get(ctx.state.client.id, provider)
     : null
-  if (res) {
-    sendJSON(ctx, res)
-  } else {
-    send500(ctx)
-  }
+  if (!res) throw createHttpError(Status.InternalServerError)
+  sendJSON(ctx, res)
 })
 
 router.delete('/:provider', addClient, requireClient, async ctx => {
@@ -60,11 +43,10 @@ router.delete('/:provider', addClient, requireClient, async ctx => {
   const res = provider
     ? await AccountController.delete(ctx.state.client.id, provider)
     : null
-  if (res) {
-    send204(ctx)
-  } else {
-    send500(ctx)
-  }
+  if (!res) throw createHttpError(Status.InternalServerError)
+
+  ctx.response.status = Status.NoContent
+  ctx.response.type = undefined
 })
 
 export default router
