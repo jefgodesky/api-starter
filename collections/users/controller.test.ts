f47a2262c45357b661ac54ddbbe3c6ae941b5e2e
Jason Godesky <jefgodesky@gmail.com>
2024-12-21 11:17:36 -0500

    UserController.get: Handles packaging response

--- collections/users/controller.test.ts
+++ collections/users/controller.test.ts
@@ -1,5 +1,6 @@
 import { describe, afterEach, afterAll, it } from '@std/testing/bdd'
 import { expect } from '@std/expect'
+import { createMockContext } from '@oak/oak/testing'
 import type Response from '../../types/response.ts'
 import type UserResource from '../../types/user-resource.ts'
 import DB from '../../DB.ts'
@@ -15,98 +16,40 @@ describe('UserController', () => {
   })
 
   const user = {
+    id: crypto.randomUUID(),
     name: 'John Doe',
     username: 'john',
   }
 
-  const fieldsets = [
-    ['name', user.name, undefined],
-    ['username', undefined, user.username],
-    ['name,username', user.name, user.username]
-  ]
-
-  const expectUser = (res: Response, name: string): void => {
-    const data = res.data as UserResource
-    expect(data).toBeDefined()
-    expect(data.type).toBe('users')
-    expect(data.attributes).toHaveProperty('name', name)
-  }
-
-  describe('getById', () => {
-    it('returns undefined if no user can be found', async () => {
-      const res = await UserController.getById(crypto.randomUUID())
-      expect(res).toBeUndefined()
-    })
-
-    it('returns the user', async () => {
-      const repository = UserController.getRepository()
-      const saved = await repository.save(user)
-      const res = await UserController.getById(saved?.id!)
-      expectUser(res!, user.name)
-    })
-
-    it('returns a sparse fieldset', async () => {
-      const repository = UserController.getRepository()
-      const saved = await repository.save(user)
-      for (const [q, name, username] of fieldsets) {
-        const url = new URL(`http://localhost:8001/v1/users/${saved?.id}?fields[users]=${q}`)
-        const res = await UserController.getById(saved?.id!, url)
-        const data = res?.data as UserResource
-        expect(data.attributes.name).toBe(name)
-        expect(data.attributes.username).toBe(username)
-      }
-    })
-  })
-
-  describe('getByUsername', () => {
-    it('returns undefined if no user can be found', async () => {
-      const res = await UserController.getByUsername(user.username)
-      expect(res).toBeUndefined()
-    })
-
-    it('returns the user', async () => {
-      const repository = UserController.getRepository()
-      const saved = await repository.save(user)
-      const res = await UserController.getByUsername(saved?.username!)
-      expectUser(res!, user.name)
-    })
-
-    it('returns a sparse fieldset', async () => {
-      const repository = UserController.getRepository()
-      const saved = await repository.save(user)
-      for (const [q, name, username] of fieldsets) {
-        const url = new URL(`http://localhost:8001/v1/users/${saved?.id}?fields[users]=${q}`)
-        const res = await UserController.getByUsername(saved?.username!, url)
-        const data = res?.data as UserResource
-        expect(data.attributes.name).toBe(name)
-        expect(data.attributes.username).toBe(username)
-      }
-    })
-  })
-
   describe('get', () => {
-    it('returns undefined if no user can be found by ID', async () => {
-      const res = await UserController.get(crypto.randomUUID())
-      expect(res).toBeUndefined()
+    const ctx = createMockContext({ state: { user } })
+
+    it('returns the user', () => {
+      UserController.get(ctx)
+      const res = ctx.response.body as Response
+      const data = res?.data as UserResource
+      expect(ctx.response.status).toBe(200)
+      expect(data).toBeDefined()
+      expect(data.type).toBe('users')
+      expect(data.attributes).toHaveProperty('name', name)
     })
 
-    it('returns undefined if no user can be found by username', async () => {
-      const res = await UserController.get(user.username)
-      expect(res).toBeUndefined()
-    })
+    it('returns a sparse fieldset', () => {
+      const fieldsets = [
+        ['name', user.name, undefined],
+        ['username', undefined, user.username],
+        ['name,username', user.name, user.username]
+      ]
 
-    it('returns a user found by ID', async () => {
-      const repository = UserController.getRepository()
-      const saved = await repository.save(user)
-      const res = await UserController.get(saved?.id!)
-      expectUser(res!, user.name)
-    })
-
-    it('returns a user found by username', async () => {
-      const repository = UserController.getRepository()
-      const saved = await repository.save(user)
-      const res = await UserController.get(saved?.username!)
-      expectUser(res!, user.name)
+      for (const [q, name, username] of fieldsets) {
+        const url = new URL(`http://localhost:8001/v1/users/${user.id}?fields[users]=${q}`)
+        UserController.get(ctx, url)
+        const res = ctx.response.body as Response
+        const data = res?.data as UserResource
+        expect(ctx.response.status).toBe(200)
+        expect(data.attributes.name).toBe(name)
+        expect(data.attributes.username).toBe(username)
+      }
     })
   })
 })
--- collections/users/controller.ts
+++ collections/users/controller.ts
@@ -1,46 +1,14 @@
-import * as uuid from '@std/uuid'
 import { Context } from '@oak/oak'
-import type Response from '../../types/response.ts'
-import type User from '../../types/user.ts'
-import UserRepository from './repository.ts'
+import sendJSON from '../../utils/responses/send-json.ts'
 import userToUserResponse from '../../utils/transformers/user-to-user-response.ts'
 import urlToUserFields from '../../utils/transformers/url-to-user-fields.ts'
 
 class UserController {
-  private static repository: UserRepository
-
-  constructor () {
-    UserController.repository = UserController.getRepository()
-  }
-
-  static getRepository (): UserRepository {
-    if (!UserController.repository) UserController.repository = new UserRepository()
-    return UserController.repository
-  }
-
-  static async get (id: string, url?: Context | URL): Promise<Response | undefined> {
-    return uuid.v4.validate(id)
-      ? await UserController.getById(id, url)
-      : await UserController.getByUsername(id, url)
-  }
-
-  static async getById (id: string, url?: Context | URL): Promise<Response | undefined> {
-    const repository = UserController.getRepository()
-    const user = await repository.get(id)
-    return UserController.getUser(user, url)
-  }
-
-  static async getByUsername (username: string, url?: Context | URL): Promise<Response | undefined> {
-    const repository = UserController.getRepository()
-    const user = await repository.getByUsername(username)
-    return UserController.getUser(user, url)
-  }
-
-  private static getUser (user: User | null, url?: Context | URL): Response | undefined {
-    const fields = url ? urlToUserFields(url) : undefined
-    return user
-      ? userToUserResponse(user, fields)
-      : undefined
+  static get (ctx: Context, url?: URL) {
+    const fieldSrc = url ?? ctx
+    const fields = urlToUserFields(fieldSrc)
+    const res = userToUserResponse(ctx.state.user, fields)
+    sendJSON(ctx, res)
   }
 }
 
