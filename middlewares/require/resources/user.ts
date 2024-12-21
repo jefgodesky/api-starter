7caf8b748f83f7c5d770d893b338a0c7d2fec814
Jason Godesky <jefgodesky@gmail.com>
2024-12-21 13:49:10 -0500

    requireUser: Rewrite to throw errors

--- middlewares/require/resources/user.ts
+++ middlewares/require/resources/user.ts
@@ -1,12 +1,10 @@
-import { Middleware } from '@oak/oak'
-import { send404 } from '../../../utils/responses/errors.ts'
+import { Middleware, Status } from '@oak/oak'
+import { createHttpError } from 'jsr:@oak/commons@1/http_errors'
+import getMessage from '../../../utils/get-message.ts'
 
 const requireUser: Middleware = async (ctx, next) => {
-  if (!ctx.state.user) {
-    send404(ctx)
-  } else {
-    await next()
-  }
+  if (!ctx.state.user) throw createHttpError(Status.NotFound, getMessage('user_not_found'))
+  await next()
 }
 
 export default requireUser
