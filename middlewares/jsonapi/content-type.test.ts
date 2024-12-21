d1816e834297bc204a195041027f67e7f969c3f3
Jason Godesky <jefgodesky@gmail.com>
2024-12-21 14:20:27 -0500

    enforceJsonApiContentType: Rewrite to throw errors

--- middlewares/jsonapi/content-type.test.ts
+++ middlewares/jsonapi/content-type.test.ts
@@ -1,13 +1,24 @@
-import { describe, it } from 'jsr:@std/testing/bdd'
+import { describe, beforeEach, it } from 'jsr:@std/testing/bdd'
 import { expect } from 'jsr:@std/expect'
-import { createMockContext, createMockNext } from '@oak/oak/testing'
+import { type Spy } from '@std/testing/mock'
+import { createMockContext } from '@oak/oak/testing'
+import { HttpError, Status } from '@oak/oak'
+import createNextSpy from '../../utils/testing/create-next-spy.ts'
+import getMessage from '../../utils/get-message.ts'
 import enforceJsonApiContentType from './content-type.ts'
+import Http = Deno.errors.Http
 
 describe('enforceJsonApiContentType', () => {
+  let next: Spy<unknown, [], Promise<void>>
+
+  beforeEach(() => {
+    next = createNextSpy()
+  })
+
   it('proceeds if there is no Content-Type header', async () => {
     const ctx = createMockContext()
-    await enforceJsonApiContentType(ctx, createMockNext())
-    expect(ctx.response.status).not.toBe(415)
+    await enforceJsonApiContentType(ctx, next)
+    expect(next.calls).toHaveLength(1)
   })
 
   it('proceeds if given a valid Content-Type header', async () => {
@@ -16,8 +27,8 @@ describe('enforceJsonApiContentType', () => {
         ['Content-Type', 'application/vnd.api+json'],
       ]
     })
-    await enforceJsonApiContentType(ctx, createMockNext())
-    expect(ctx.response.status).not.toBe(415)
+    await enforceJsonApiContentType(ctx, next)
+    expect(next.calls).toHaveLength(1)
   })
 
   it('returns 415 if not given a valid Content-Type header', async () => {
@@ -26,8 +37,15 @@ describe('enforceJsonApiContentType', () => {
         ['Content-Type', 'application/json'],
       ]
     })
-    await enforceJsonApiContentType(ctx, createMockNext())
-    expect(ctx.response.status).toBe(415)
+
+    try {
+      await enforceJsonApiContentType(ctx, next)
+      expect(0).toBe('Should throw 415 error when given an invalid Content-Type header')
+    } catch (err) {
+      expect((err as HttpError).status).toBe(Status.UnsupportedMediaType)
+      expect((err as HttpError).message).toBe(getMessage('jsonapi_enforce_content_type'))
+      expect(next.calls).toHaveLength(0)
+    }
   })
 
   it('returns 415 if given an invalid profile', async () => {
@@ -36,8 +54,15 @@ describe('enforceJsonApiContentType', () => {
         ['Content-Type', 'application/vnd.api+json;profile="https://example.com/resource-timestamps"'],
       ]
     })
-    await enforceJsonApiContentType(ctx, createMockNext())
-    expect(ctx.response.status).toBe(415)
+
+    try {
+      await enforceJsonApiContentType(ctx, next)
+      expect(0).toBe('Should throw 415 error when given an invalid profile')
+    } catch (err) {
+      expect((err as HttpError).status).toBe(Status.UnsupportedMediaType)
+      expect((err as HttpError).message).toBe(getMessage('jsonapi_enforce_content_type'))
+      expect(next.calls).toHaveLength(0)
+    }
   })
 
   it('returns 415 if given an invalid extension', async () => {
@@ -46,8 +71,15 @@ describe('enforceJsonApiContentType', () => {
         ['Content-Type', 'application/vnd.api+json;ext="https://jsonapi.org/ext/version"'],
       ]
     })
-    await enforceJsonApiContentType(ctx, createMockNext())
-    expect(ctx.response.status).toBe(415)
+
+    try {
+      await enforceJsonApiContentType(ctx, next)
+      expect(0).toBe('Should throw 415 error when given an invalid extension')
+    } catch (err) {
+      expect((err as HttpError).status).toBe(Status.UnsupportedMediaType)
+      expect((err as HttpError).message).toBe(getMessage('jsonapi_enforce_content_type'))
+      expect(next.calls).toHaveLength(0)
+    }
   })
 
   it('returns 415 if given any other parameter', async () => {
@@ -56,7 +88,14 @@ describe('enforceJsonApiContentType', () => {
         ['Content-Type', 'application/vnd.api+json;other="hello"'],
       ]
     })
-    await enforceJsonApiContentType(ctx, createMockNext())
-    expect(ctx.response.status).toBe(415)
+
+    try {
+      await enforceJsonApiContentType(ctx, next)
+      expect(0).toBe('Should throw 415 error when given an invalid Content-Type parameter')
+    } catch (err) {
+      expect((err as HttpError).status).toBe(Status.UnsupportedMediaType)
+      expect((err as HttpError).message).toBe(getMessage('jsonapi_enforce_content_type'))
+      expect(next.calls).toHaveLength(0)
+    }
   })
 })
--- middlewares/jsonapi/content-type.ts
+++ middlewares/jsonapi/content-type.ts
@@ -1,14 +1,11 @@
-import { Middleware } from '@oak/oak'
-import { send415 } from '../../utils/responses/errors.ts'
+import { Middleware, Status, createHttpError } from '@oak/oak'
 import isValidMediaType from '../../utils/is-valid-media-type.ts'
+import getMessage from '../../utils/get-message.ts'
 
 const enforceJsonApiContentType: Middleware = async (ctx, next) => {
   const contentType = ctx.request.headers.get('Content-Type')
-  if (!contentType || isValidMediaType(contentType)) {
-    await next()
-  } else {
-    send415(ctx)
-  }
+  if (contentType && !isValidMediaType(contentType)) throw createHttpError(Status.UnsupportedMediaType, getMessage('jsonapi_enforce_content_type'))
+  await next()
 }
 
 export default enforceJsonApiContentType
