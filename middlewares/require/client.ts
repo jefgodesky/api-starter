2db906dc5b8300e8c218f865c7ec22c1eb71966b
Jason Godesky <jefgodesky@gmail.com>
2024-12-21 13:45:04 -0500

    requireClient: Rewrite to throw errors

--- middlewares/require/client.ts
+++ middlewares/require/client.ts
@@ -1,12 +1,10 @@
-import { Middleware } from '@oak/oak'
-import { send401 } from '../../utils/responses/errors.ts'
+import { Middleware, Status } from '@oak/oak'
+import { createHttpError } from 'jsr:@oak/commons@1/http_errors'
+import getMessage from '../../utils/get-message.ts'
 
 const requireClient: Middleware = async (ctx, next) => {
-  if (!ctx.state.client) {
-    send401(ctx)
-  } else {
-    await next()
-  }
+  if (!ctx.state.client) throw createHttpError(Status.Unauthorized, getMessage('authentication_required'))
+  await next()
 }
 
 export default requireClient
