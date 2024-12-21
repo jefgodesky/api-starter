c2cad9b1b7e18d78b1233bd5c4a4af57cdaa679b
Jason Godesky <jefgodesky@gmail.com>
2024-12-21 14:26:54 -0500

    /auth/tokens — Rewrite to throw errors

--- collections/auth/tokens/router.ts
+++ collections/auth/tokens/router.ts
@@ -1,8 +1,8 @@
-import { Router } from '@oak/oak'
+import { Router, Status, createHttpError } from '@oak/oak'
 import AuthTokenController from './controller.ts'
 import sendJSON from '../../../utils/responses/send-json.ts'
-import { send400 } from '../../../utils/responses/errors.ts'
 import requireTokenCreationBody from '../../../middlewares/require/body/token-creation.ts'
+import getMessage from '../../../utils/get-message.ts'
 
 const router = new Router({
   prefix: '/tokens'
@@ -11,18 +11,11 @@ const router = new Router({
 router.post('/', requireTokenCreationBody, async ctx => {
   const body = await ctx.request.body.json()
   const { provider, token } = body.data.attributes
-  try {
-    const res = provider
-      ? await AuthTokenController.create(provider, token)
-      : await AuthTokenController.refresh(token)
-    if (res) {
-      sendJSON(ctx, res)
-    } else {
-      send400(ctx)
-    }
-  } catch (_) {
-    send400(ctx)
-  }
+  const res = provider
+    ? await AuthTokenController.create(provider, token)
+    : await AuthTokenController.refresh(token)
+  if (!res) throw createHttpError(Status.BadRequest, getMessage('invalid_auth_token'))
+  sendJSON(ctx, res)
 })
 
 export default router
