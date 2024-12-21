e1e61f52ad14862d27596a21fbd3f2f6c3eef245
Jason Godesky <jefgodesky@gmail.com>
2024-12-21 11:17:54 -0500

    GET /v1/users/:userId — Just calls UserController.get

--- collections/users/router.ts
+++ collections/users/router.ts
@@ -1,20 +1,15 @@
 import { Router } from '@oak/oak'
 import UserController from './controller.ts'
+import addResource from '../../middlewares/add-resource.ts'
+import requireUser from '../../middlewares/require/resources/user.ts'
 import getPrefix from '../../utils/get-prefix.ts'
-import sendJSON from '../../utils/responses/send-json.ts'
-import { send404 } from '../../utils/responses/errors.ts'
 
 const router = new Router({
   prefix: getPrefix('users')
 })
 
-router.get('/:userId', async ctx => {
-  const user = await UserController.get(ctx.params.userId, ctx)
-  if (user) {
-    sendJSON(ctx, user)
-  } else {
-    send404(ctx)
-  }
+router.get('/:userId', addResource, requireUser, ctx => {
+  UserController.get(ctx)
 })
 
 export default router
