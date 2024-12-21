3fac8e6d53cec7e0ce9bbe8db499516d610a40dd
Jason Godesky <jefgodesky@gmail.com>
2024-12-21 13:29:24 -0500

    Add new type: ErrorObject

--- types/response.ts
+++ types/response.ts
@@ -1,4 +1,5 @@
 import JSONAPI from './json-api.ts'
+import ErrorObject from './error-object.ts'
 import Links from './links.ts'
 import Resource from './resource.ts'
 
@@ -6,5 +7,6 @@ export default interface Response {
   jsonapi: JSONAPI
   links: Links
   data?: Resource | Resource[]
+  errors?: ErrorObject[]
   included?: Resource[]
 }
