40e5bd1b215a4095ec6f875bf388f22bd7276a54
Jason Godesky <jefgodesky@gmail.com>
2024-12-23 10:19:09 -0500

    Add new middleware: loadAccount

--- middlewares/load/resource.ts
+++ middlewares/load/resource.ts
@@ -1,10 +1,12 @@
 import { Middleware } from '@oak/oak'
+import loadAccount from './account.ts'
 import loadUser from './user.ts'
 
 const loadResource: Middleware = async (ctx, next) => {
-  if (ctx.state.params?.userId) {
-    await loadUser(ctx, async () => {})
-  }
+  const { client, params } = ctx.state
+
+  if (client && params?.provider) await loadAccount(ctx, async () => {})
+  if (params?.userId) await loadUser(ctx, async () => {})
 
   await next()
 }
