6c7f351bd2fbaac0ebb96ee6d45873b3c7e035af
Jason Godesky <jefgodesky@gmail.com>
2024-12-23 10:23:52 -0500

    Add new middleware: requireAccount

--- middlewares/require/resources/account.test.ts
+++ middlewares/require/resources/account.test.ts
@@ -2,30 +2,31 @@ import { describe, it } from 'jsr:@std/testing/bdd'
 import { expect } from 'jsr:@std/expect'
 import { HttpError, Status } from '@oak/oak'
 import { createMockContext } from '@oak/oak/testing'
+import { PROVIDERS } from '../../../types/provider.ts'
 import createNextSpy from '../../../utils/testing/create-next-spy.ts'
 import getMessage from '../../../utils/get-message.ts'
-import requireUser from './user.ts'
+import requireAccount from './account.ts'
 
-describe('requireUser', () => {
-  it('proceeds if there is a user resource in state', async () => {
+describe('requireAccount', () => {
+  it('proceeds if there is an account resource in state', async () => {
     const ctx = createMockContext({
-      state: { user: { name: 'John Doe' } }
+      state: { account: { provider: PROVIDERS.GOOGLE, uid: crypto.randomUUID(), pid: '1' } }
     })
 
     const next = createNextSpy()
-    await requireUser(ctx, next)
+    await requireAccount(ctx, next)
     expect(next.calls).toHaveLength(1)
   })
 
-  it('returns 404 if there is no user', async () => {
+  it('returns 404 if there is no account', async () => {
     const ctx = createMockContext()
     const next = createNextSpy()
 
     try {
-      await requireUser(ctx, next)
-      expect(0).toBe('No user found should throw a 404 error.')
+      await requireAccount(ctx, next)
+      expect(0).toBe('No account found should throw a 404 error.')
     } catch (err) {
-      expect((err as HttpError).message).toBe(getMessage('user_not_found'))
+      expect((err as HttpError).message).toBe(getMessage('account_not_found'))
       expect((err as HttpError).status).toBe(Status.NotFound)
       expect(next.calls).toHaveLength(0)
     }
