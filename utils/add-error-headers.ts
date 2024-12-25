e989bf34d93f65972c6f5b0d062630747ae9e4f0
Jason Godesky <jefgodesky@gmail.com>
2024-12-25 14:28:00 -0500

    addErrorHeaders: Add Allow, Access-Control-Allow-Methods to 405

--- utils/add-error-headers.ts
+++ utils/add-error-headers.ts
@@ -1,9 +1,14 @@
-import { Context, Status } from '@oak/oak'
+import { Context, Status, Router } from '@oak/oak'
+import getAllowedMethods from './get-allowed-methods.ts'
 
-const addErrorHeaders = (ctx: Context) => {
+const addErrorHeaders = (ctx: Context, routers: Record<string, Router>) => {
   switch (ctx.response.status) {
     case Status.Unauthorized:
       ctx.response.headers.set('WWW-Authenticate', 'Bearer')
+      break
+    case Status.MethodNotAllowed:
+      ctx.response.headers.set('Allow', getAllowedMethods(ctx.request.url.pathname, routers).join(', '))
+      ctx.response.headers.set('Access-Control-Allow-Methods', getAllowedMethods(ctx.request.url.pathname, routers).join(', '))
       break
     default:
       break
