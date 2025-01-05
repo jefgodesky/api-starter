68056777437b28f2fce873a6251bea2633caa2f5
Jason Godesky <jefgodesky@gmail.com>
2025-01-05 13:49:43 -0500

    userToLink: Refactor unit tests

--- utils/transformers/user-to-link.test.ts
+++ utils/transformers/user-to-link.test.ts
@@ -4,23 +4,18 @@ import type User from '../../types/user.ts'
 import userToLink from './user-to-link.ts'
 
 describe('userToLink', () => {
-  it('returns a link to a UserResource', () => {
-    const user: User = {
-      id: crypto.randomUUID(),
-      name: 'John Doe',
-      username: 'john'
-    }
+  const user: User = {
+    name: 'John Doe',
+    username: 'john'
+  }
 
-    const expected = `http://localhost:8001/v1/users/${user.id}`
-    expect(userToLink(user)).toBe(expected)
+  it('returns a link to a UserResource', () => {
+    const id = crypto.randomUUID()
+    const expected = `http://localhost:8001/v1/users/${id}`
+    expect(userToLink({ id, ...user })).toBe(expected)
   })
 
   it('returns a link to the Users collection if user has no ID', () => {
-    const user: User = {
-      name: 'John Doe',
-      username: 'john'
-    }
-
     const expected = 'http://localhost:8001/v1/users'
     expect(userToLink(user)).toBe(expected)
   })
