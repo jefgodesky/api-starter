d55ab48d58d3abc03a8fe9b131504261bc23c1cd
Jason Godesky <jefgodesky@gmail.com>
2025-01-11 11:11:54 -0500

    userToUserAttributes: Use getAllFieldCombinations to improve unit tests

--- utils/transformers/user-to-user-attributes.test.ts
+++ utils/transformers/user-to-user-attributes.test.ts
@@ -1,21 +1,15 @@
 import { describe, it } from 'jsr:@std/testing/bdd'
 import { expect } from 'jsr:@std/expect'
 import type User from '../../types/user.ts'
-import { type UserAttributesKeys } from '../../types/user-attributes.ts'
+import { type UserAttributesKeys, allUserAttributes } from '../../types/user-attributes.ts'
+import getAllFieldCombinations from '../testing/get-all-field-combinations.ts'
 import userToUserAttributes from './user-to-user-attributes.ts'
 
 describe('userToUserAttributes', () => {
   const user: User = {
-    id: crypto.randomUUID(),
     name: 'John Doe',
     username: 'john'
   }
-
-  const fieldsets: [UserAttributesKeys[], UserAttributesKeys[], string[]][] = [
-    [['name'], ['username'], [user.name]],
-    [['username'], ['name'], [user.username ?? '']],
-    [['name', 'username'], [], [user.name, user.username ?? '']]
-  ]
 
   it('returns a UserAttributes object', () => {
     const actual = userToUserAttributes(user)
@@ -25,10 +19,14 @@ describe('userToUserAttributes', () => {
   })
 
   it('can return a sparse fieldset', () => {
-    for (const [included, excluded, expected] of fieldsets) {
-      const actual = userToUserAttributes(user, included)
-      expect(Object.keys(actual)).toHaveLength(included.length)
-      for (let i = 0; i < included.length; i++) expect(actual[included[i]]).toBe(expected[i])
+    const objects = getAllFieldCombinations(user)
+    for (const object of objects) {
+      const fields = Object.keys(object) as UserAttributesKeys[]
+      const excluded = allUserAttributes.filter(attr => !fields.includes(attr))
+      const actual = userToUserAttributes(user, fields)
+
+      expect(Object.keys(actual)).toHaveLength(fields.length)
+      for (const field of fields) expect(actual[field]).toBe(user[field])
       for (const ex of excluded) expect(actual[ex]).not.toBeDefined()
     }
   })
