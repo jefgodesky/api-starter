fde89cc651353bf970355a74c0e6fbc0c4de9356
Jason Godesky <jefgodesky@gmail.com>
2025-01-11 11:15:47 -0500

    userToUserResource: Use getAllFieldCombinations to improve unit tests

--- utils/transformers/user-to-user-resource.test.ts
+++ utils/transformers/user-to-user-resource.test.ts
@@ -1,21 +1,21 @@
 import { describe, it } from 'jsr:@std/testing/bdd'
 import { expect } from 'jsr:@std/expect'
 import type User from '../../types/user.ts'
-import type { UserAttributesKeys}  from '../../types/user-attributes.ts'
+import UserAttributes, { type UserAttributesKeys, allUserAttributes }  from '../../types/user-attributes.ts'
+import getAllFieldCombinations from '../testing/get-all-field-combinations.ts'
 import userToUserResource from './user-to-user-resource.ts'
 
 describe('userToUserResource', () => {
-  const user: User = {
-    id: crypto.randomUUID(),
+  const attributes: UserAttributes = {
     name: 'John Doe',
-    username: 'john'
+    username: 'john',
   }
 
-  const fieldsets: [UserAttributesKeys[], UserAttributesKeys[], string[]][] = [
-    [['name'], ['username'], [user.name]],
-    [['username'], ['name'], [user.username ?? '']],
-    [['name', 'username'], [], [user.name, user.username ?? '']]
-  ]
+  const user: User = {
+    id: crypto.randomUUID(),
+    name: attributes.name!,
+    username: attributes.username
+  }
 
   it('returns a UserResource object', () => {
     const actual = userToUserResource(user)
@@ -31,12 +31,14 @@ describe('userToUserResource', () => {
   })
 
   it('can return a sparse fieldset', () => {
-    for (const [included, excluded, expected] of fieldsets) {
-      const actual = userToUserResource(user, included)
-      expect(actual.type).toBe('users')
-      expect(actual.id).toBe(user.id)
-      expect(Object.keys(actual.attributes)).toHaveLength(included.length)
-      for (let i = 0; i < included.length; i++) expect(actual.attributes[included[i]]).toBe(expected[i])
+    const objects = getAllFieldCombinations(attributes)
+    for (const object of objects) {
+      const fields = Object.keys(object) as UserAttributesKeys[]
+      const excluded = allUserAttributes.filter(attr => !fields.includes(attr))
+      const actual = userToUserResource(user, fields)
+
+      expect(Object.keys(actual.attributes)).toHaveLength(fields.length)
+      for (const field of fields) expect(actual.attributes[field]).toBe(user[field])
       for (const ex of excluded) expect(actual.attributes[ex]).not.toBeDefined()
     }
   })
