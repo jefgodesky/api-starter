31f9a35fc2b29e9e8f9823706fdb69748618766c
Jason Godesky <jefgodesky@gmail.com>
2025-02-05 20:40:14 -0500

    Add isAuthTokenAttributes

--- types/auth-token-attributes.ts
+++ types/auth-token-attributes.ts
@@ -1,4 +1,17 @@
+import isObject from '../utils/guards/object.ts'
+
 export default interface AuthTokenAttributes {
   token: string
   expiration: string
 }
+
+const isAuthTokenAttributes = (candidate: unknown): candidate is AuthTokenAttributes => {
+  if (!isObject(candidate)) return false
+  const obj = candidate as Record<string, unknown>
+
+  const fields = ['token', 'expiration']
+  if (!fields.every(key => typeof obj[key] === 'string')) return false
+  return Object.keys(obj).every(key => fields.includes(key))
+}
+
+export { isAuthTokenAttributes }
