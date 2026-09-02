f733d1da2befc41ed6bffc01c5748813ba6e31dd
Jason Godesky <jefgodesky@gmail.com>
2026-09-01 21:53:38 -0400

    Run API

--- env/test.sh
+++ env/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "🏁 Starting test environment..."
+docker compose -f env/test.yml down -v
 docker compose -f env/test.yml up --build -d
 
 echo ""
@@ -11,4 +12,4 @@ TEST_EXIT_CODE=$(docker inspect tests --format='{{.State.ExitCode}}')
 
 echo ""
 echo -e "🧹 Cleaning up..."
-docker compose -f env/test.yml down
+docker compose -f env/test.yml down -v
