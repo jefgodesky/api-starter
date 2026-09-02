3a16cb2e3b48b00266d17bd15d5ed22ad7840736
Jason Godesky <jefgodesky@gmail.com>
2026-09-02 19:54:51 -0400

    Add API version as base path

--- api.ts
+++ api.ts
@@ -1,6 +1,8 @@
 import { Hono } from 'hono'
+import getEnvNumber from './utils/get-env-num.ts'
 
-const api = new Hono()
+const v = getEnvNumber('API_VERSION', 1)
+const api = new Hono().basePath(`v${v}`)
 
 api.get('/', (c) => c.text('Hello, world!'))
 
