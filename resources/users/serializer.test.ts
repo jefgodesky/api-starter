a43df5e2b73e9ce09e4d5b225f5b539c527d83c2
Jason Godesky <jefgodesky@gmail.com>
2026-09-07 19:05:09 -0400

    Add new serializer: UserSerializer

---
+++ resources/users/serializer.test.ts
@@ -0,0 +1,50 @@
+import { describe, it } from 'node:test'
+import { expect } from '@std/expect'
+import { type User } from './db.ts'
+import { serializeUser } from './serializer.ts'
+
+const id = crypto.randomUUID()
+const name = 'John Doe'
+const username = 'john'
+const user: User = { id, name, username }
+
+const getDoc = async (
+  fields?: string[],
+  // deno-lint-ignore no-explicit-any
+): Promise<Record<string, any> | null> => {
+  const doc = await serializeUser(user, fields)
+  const resource = doc.data
+  if (!resource) return null
+  if (Array.isArray(resource)) return null
+  if (!('attributes' in resource)) return null
+  return { ...doc }
+}
+
+describe('serializeUser', () => {
+  it('serializes attributes', async () => {
+    const doc = await getDoc()
+    expect(doc?.data?.attributes).toEqual({ name, username })
+  })
+
+  it('supports sparse fieldset', async () => {
+    const doc = await getDoc(['name'])
+    expect(doc?.data?.attributes).toEqual({ name })
+  })
+
+  it('can be narrowed down to no attributes', async () => {
+    const doc = await getDoc([])
+    expect(doc?.data?.attributes).toEqual({})
+  })
+
+  it('adds self link', async () => {
+    const doc = await getDoc()
+    expect(doc?.data?.type).toBe('users')
+    expect(doc?.data?.id).toBe(id)
+    expect(doc?.data?.links?.self).toMatch(new RegExp(`/users/${id}$`))
+  })
+
+  it('sets JSON:API v1.1', async () => {
+    const doc = await getDoc()
+    expect(doc?.jsonapi?.version).toBe('1.1')
+  })
+})
---
+++ resources/users/serializer.ts
@@ -0,0 +1,16 @@
+import jsonapi from 'ts-japi'
+import { type User } from './db.ts'
+import getRoot from '../../utils/root.ts'
+
+const { Serializer, Linker } = jsonapi
+
+export const UserSerializer = new Serializer<User>('users', {
+  version: '1.1',
+  linkers: { resource: new Linker((u: User) => `${getRoot()}/users/${u.id}`) },
+})
+
+const projectionFor = (fields?: string[]) =>
+  fields === undefined ? null : Object.fromEntries(fields.map((f) => [f, 1]))
+
+export const serializeUser = (user: User, fields?: string[]) =>
+  UserSerializer.serialize(user, { projection: projectionFor(fields) })
