28396a556ec67415f8b970252f1d7849692b2b99
Jason Godesky <jefgodesky@gmail.com>
2026-09-07 15:19:40 -0400

    createMiddlewareTest: Expand to handle URL

--- utils/testing/middleware.ts
+++ utils/testing/middleware.ts
@@ -3,13 +3,14 @@ import { Hono, type MiddlewareHandler } from 'hono'
 const createMiddlewareTest = (
   mw: MiddlewareHandler,
   header: string,
-  method = 'GET',
+  method: string = 'GET',
+  url: URL = new URL('https://example.com/v1/test'),
 ) => {
   const app = new Hono()
   app.use('*', mw)
-  app.on(method, '/', (c) => c.body(null, 204))
+  app.on(method, url.pathname, (c) => c.body(null, 204))
   return (value?: string): Promise<Response> | Response =>
-    app.request('/', {
+    app.request(url.toString(), {
       method,
       headers: value ? { [header]: value } : undefined,
     })
