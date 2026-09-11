2b67e1fa9b6a1d3c3e7e7c5d00c61e31b3cd1c39
Jason Godesky <jefgodesky@gmail.com>
2026-09-11 18:46:29 -0400

    Add tokens, accounts to Database interface

--- db/tables.ts
+++ db/tables.ts
@@ -1,5 +1,9 @@
+import type { AccountsData } from '../resources/accounts/db/types.ts'
+import type { TokensData } from '../resources/auth/tokens/db/types.ts'
 import type { UsersData } from '../resources/users/db/types.ts'
 
 export interface Database {
+  accounts: AccountsData
+  tokens: TokensData
   users: UsersData
 }
